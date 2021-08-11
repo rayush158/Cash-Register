ee87082a22d8a78c4bd87364f923a5dee0e66ddb
ayushrajput158 <87794509+ayushrajput158@users.noreply.github.com>
2021-08-12 00:51:45 +0530

    intial

---
+++ index.js
@@ -0,0 +1,35 @@
+const billAmount = document.querySelector("#bill-amount");
+const cashGiven = document.querySelector("#cash-given");
+const checkButton = document.querySelector("#check-button");
+const message = document.querySelector("#error-message");
+const availableNotes = [2000, 500, 100, 50, 20, 10, 5, 1];
+const noOfNotes = document.querySelectorAll(".no-of-notes");
+checkButton.addEventListener("click", function validateBillAndCashAmount() {
+  hideMessage();
+  if (billAmount.value > 0) {
+    if (cashGiven.value >= billAmount.value) {
+      const amountToBeReturned = cashGiven.value - billAmount.value;
+      calculateChange(amountToBeReturned);
+    } else {
+      showMessage("Do You wanna wash plates?");
+    }
+  } else {
+    showMessage("Invalid Bill Amount");
+  }
+});
+function calculateChange(amountToBeReturned) {
+  for (let i = 0; i < availableNotes.length; i++) {
+    const numberOfNotes = Math.trunc(amountToBeReturned / availableNotes[i]);
+    amountToBeReturned = amountToBeReturned % availableNotes[i];
+    noOfNotes[i].innerText = numberOfNotes;
+  }
+}
+
+function hideMessage() {
+  message.style.display = "none";
+}
+
+function showMessage(msg) {
+  message.style.display = "block";
+  message.innerText = msg;
+}
