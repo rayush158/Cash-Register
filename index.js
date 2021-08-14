e8d55026524ae95cf5efe28b504e67baacf97a31
Ayush Kumar <87794509+ayushrajput158@users.noreply.github.com>
2021-08-14 19:30:29 +0530

    fix: parse added

--- index.js
+++ index.js
@@ -6,9 +6,11 @@ const availableNotes = [2000, 500, 100, 50, 20, 10, 5, 1];
 const noOfNotes = document.querySelectorAll(".no-of-notes");
 checkButton.addEventListener("click", function validateBillAndCashAmount() {
   hideMessage();
-  if (billAmount.value > 0) {
-    if (cashGiven.value >= billAmount.value) {
-      const amountToBeReturned = cashGiven.value - billAmount.value;
+  let billMoney = Number(billAmount.value)
+  let cashMoney = Number(cashGiven.value)
+  if (billMoney > 0) {
+    if (cashMoney >= billMoney) {
+      const amountToBeReturned = cashMoney - billMoney;
       calculateChange(amountToBeReturned);
     } else {
       showMessage("Do You wanna wash plates?");
